a5db64f32a45ef0a705adc2be0d86509bc6c26e9
Nickolay Rusanov <nikolarusano92@gmail.com>
2022-04-06 18:00:47 +0300

    HW1_done

---
+++ src/HW1/HW1Class.java
@@ -0,0 +1,17 @@
+package HW1;
+
+public class HW1Class {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\tРУБАИ");
+        System.out.println("Ты ночь моей надежды сделай днём,");
+        System.out.println("Пусть рок мой ходит с поднятым челом,");
+        System.out.println("Пусть, как Лейли, мой стих блеснёт лучом,");
+        System.out.println("И, как Меджнун, он сердце жжёт огнём");
+        System.out.println("\t\t\t\t\t\tO. ХАЙАМ");
+
+
+
+                ;
+    }
+}
